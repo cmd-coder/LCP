759a2a7f5db1891ca222473062886546f73b7ea3
CMD <gautamkailashsingh@gmail.com>
2020-09-27 18:58:10 +0530

    [Gautam] Added length method

---
+++ master_branch.cs
@@ -0,0 +1,11 @@
+class program
+{
+	static void main()
+	{
+		Console.WriteLine("Welcome to Comparision Computation Program");
+	}
+	static void length(int x1, int y1, int x2, int y2)
+	{
+		return Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+	}
+}
