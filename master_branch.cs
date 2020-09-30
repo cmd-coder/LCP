0d8cff4c7e1801139b52c4cddf11c10d3ee74c5c
CMD <gautamkailashsingh@gmail.com>
2020-09-30 14:57:17 +0530

    [Gautam] Added length calculation feature

--- master_branch.cs
+++ master_branch.cs
@@ -3,17 +3,13 @@ class program
 	static void main()
 	{
 		Console.WriteLine("Welcome to Comparision Computation Program");
-	}
-	static void length(int x1, int y1, int x2, int y2)
-	{
-		return Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-	}
-	static void equality(int length1, int length2)
-	{
-		return (length1==length2);
-	}
-	static int compare(int length1, int length2)
-	{
-		return length1.CompareTo(length2);
+		Console.WriteLine("Enter x1, y1, x2 and y2 one after the another");
+		int x1=Convert.ToInt32(Console.ReadLine());
+		int y1=Convert.ToInt32(Console.ReadLine());
+		int x2=Convert.ToInt32(Console.ReadLine());
+		int y2=Convert.ToInt32(Console.ReadLine());
+
+		double length=Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+		Console.WriteLine("Length of the line segment is: "+length);
 	}
 }
